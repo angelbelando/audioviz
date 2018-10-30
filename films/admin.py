3822296989e79aa4fab468cf89139c62f9ddaac7
git1 <git1@ubunts git config --global user.name git1 git config --global user.email git1@ubunts>
2018-10-30 15:17:05 +0100

    Création du modèle

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Film, Category_Film, Acteur, Role, Role_Film, Video_Type, Video, Photo
+admin.site.register(Film)
+admin.site.register(Category_Film)
+admin.site.register(Acteur)
+admin.site.register(Role)
+admin.site.register(Role_Film)
+admin.site.register(Video_Type)
+admin.site.register(Video)
+admin.site.register(Photo)
--- films/models.py
+++ films/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+class Category_Film(models.Model):
+    category = models.CharField('catégorie', max_length=32, unique=True)
 
-# Create your models here.
+    class Meta:
+        verbose_name = "catégorie"
+    def __str__(self):
+        return self.category
+
+class Acteur(models.Model):
+    name = models.CharField('nom', max_length=64, unique=True)
+    resumeCV = models.TextField("Résumé CV")
+
+    class Meta:
+        verbose_name = "Acteur"
+    def __str__(self):
+        return self.name
+
+class Role(models.Model):
+    name = models.CharField('nom', max_length=64, unique=True)
+    description = models.TextField("Description")
+    class Meta:
+        verbose_name = "Role"
+    def __str__(self):
+        return self.name
+
+
+class Video_Type(models.Model):
+    name = models.CharField('nom', max_length=40, unique=True)
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    name = models.CharField('nom', max_length=64, unique=True)
+    videoType = models.ForeignKey(Video_Type, on_delete=models.CASCADE)
+    UrlVideo = models.URLField("URL de la vidéo")
+    def __str__(self):
+        return self.name
+class Photo(models.Model):
+    name = models.CharField('nom', max_length=60, unique=True)
+    photo = models.ImageField("Photo" )
+    def __str__(self):
+        return self.name
+class Film(models.Model):
+    title = models.CharField('titre du film', max_length=96, unique=True)
+    category = models.ForeignKey(Category_Film, on_delete=models.CASCADE)
+    synopsis = models.TextField('synopsis')
+    created_at = models.DateTimeField('date de création', auto_now_add=True)
+    created_user = models.CharField("utilisateur qui a créé", max_length=32)
+    changed_at = models.DateTimeField('date de modification', auto_now_add=True)
+    changed_user = models.CharField("utilisateur qui a modifié", max_length=32)
+    picture = models.ImageField("Affiche" )
+    acteur = models.ManyToManyField(Acteur,
+        through='Role_Film',
+        through_fields=('film', 'acteur'))
+    # acteur = models.ManyToManyField(Acteur, related_name='filmsActeurs', blank=True)
+    Video = models.ManyToManyField(Video, related_name='filmsVideos', blank=True)
+    photo = models.ManyToManyField(Photo, related_name='filmsPhotos', blank=True)
+    class Meta:
+        verbose_name = "film"
+    def __str__(self):
+        return self.title
+
+class Role_Film(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    acteur = models.ForeignKey(Acteur, on_delete=models.CASCADE)
+    roledescription = models.CharField("Role Description", max_length=64, default = "")
+    class Meta:
+        verbose_name = "film/Acteur/rôle"
+    def __str__(self):
+        return self.roledescription
