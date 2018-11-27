b8f3633820f0729dbad608f49efd220362dbe8a3
git1 <git1@ubunts git config --global user.name git1 git config --global user.email git1@ubunts>
2018-11-27 14:29:13 +0100

    photos

--- audioviz/settings.py
+++ audioviz/settings.py
@@ -141,7 +141,7 @@ if os.environ.get('ENV') == 'PROD':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'), os.path.join(BASE_DIR, 'media')
+        os.path.join(PROJECT_ROOT, 'static'),
     )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
