6b15cec01b0ea1bb26a73f0c8798a7519e609d43
git1 <git1@ubunts git config --global user.name git1 git config --global user.email git1@ubunts>
2018-11-27 12:57:02 +0100

    Photos

--- audioviz/settings.py
+++ audioviz/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'films/static/media/')
 
 if os.environ.get('ENV') == 'PROD':
     
